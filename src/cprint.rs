d50670a228edfd461784df09afcb045487aa3bb3
Sellig6792 <sellig6792@gmail.com>
2023-01-16 18:02:36 +0100

    Add cprint macro

---
+++ src/cprint.rs
@@ -0,0 +1,21 @@
+use super::color::{Color, Coloration};
+use std::sync::Arc;
+
+#[macro_export]
+macro_rules! cprint {
+    ($title:expr, $msg:expr, $color:expr) => {
+        print!(
+            "{} {}",
+            format!("{}{}", " ".repeat(12 - $title.len()), $title).apply_color($color),
+            $msg
+        );
+    };
+}
+
+#[macro_export]
+macro_rules! cprintln {
+    ($title:expr, $msg:expr, $color:expr) => {
+        cprint!($title, $msg, $color);
+        println!();
+    };
+}
