f64094aa1dccbabc2c4d1e26d5c6f5d17d0673dc
Sellig6792 <sellig6792@gmail.com>
2023-01-22 16:14:00 +0100

    Document the crate

--- src/cprint_macros.rs
+++ src/cprint_macros.rs
@@ -1,3 +1,11 @@
+/// Print a message like Cargo does. The first argument is the title of the message, the second argument is the message itself and the third argument is the color of the title.
+/// The end of the title is padded with spaces to make it 12 characters long.
+/// # Examples
+/// ```rust
+/// use cprint::{cprint, Color};
+///
+/// cprint!("Compiling", "main.rs", Color::Green);
+/// ```
 #[macro_export]
 macro_rules! cprint {
     ($title:expr, $msg:expr, $color:expr) => {
@@ -9,6 +17,7 @@ macro_rules! cprint {
     };
 }
 
+/// Same as [`cprint!`] but with a newline at the end.
 #[macro_export]
 macro_rules! cprintln {
     ($title:expr, $msg:expr, $color:expr) => {
