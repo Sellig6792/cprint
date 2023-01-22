57bfe48ef99c58ab1dcd540556ebb3fa1deb026b
Sellig6792 <sellig6792@gmail.com>
2023-01-22 16:14:00 +0100

    Document the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+#![warn(missing_docs)]
+//! > **Cargo-like printing**
+//!
+//! Easily print beautiful formatted messages like Cargo does.
+//!
+//! ## Examples
+//! ```rust
+//! use cprint::{cprint, Color};
+//!
+//! cprint!("Using", "cprint crate!", Color::Green);
+//! ```
+
 mod color;
 mod cprint_macros;
 
