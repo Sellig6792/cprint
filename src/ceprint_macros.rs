5c13c1cd6bdf46d8c84a23cd2cc62006b367d3cb
Sellig6792 <sellig6792@gmail.com>
2023-01-28 11:56:13 +0100

    Add ceprint feature. This is a shortcut to show an error like Cargo

---
+++ src/ceprint_macros.rs
@@ -0,0 +1,28 @@
+use crate::{Color, Coloration};
+
+/// Print an error like Cargo does. The argument is the message of the error.
+/// The end of the title is padded with spaces to make it 12 characters long.
+/// # Examples
+/// ```rust
+/// use cprint::{ceprint, Color, Coloration};
+///
+/// ceprint!("Failed to compile");
+/// ```
+#[macro_export]
+macro_rules! ceprint {
+    ($msg:expr) => {
+        print!(
+            "{} {}",
+            format!("{}{}", " ".repeat(12 - "error".len()), "Error").apply_color(Color::Red),
+            $msg
+        );
+    };
+}
+
+/// Same as [`ceprint!`] but with a newline at the end.
+#[macro_export]
+macro_rules! ceprintln {
+    ($title:expr, $msg:expr) => {
+        ceprintln!($title, $msg, Color::Red);
+    };
+}
