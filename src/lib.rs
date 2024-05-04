6c41db0d3e5a32b8a8bde9fe2c564d11c75c4992
Gilles Meunier <sellig6792@gmail.com>
2024-05-04 12:46:09 +0200

    Edit docs to suit new or edited features

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! > **Cargo-like printing**
+//! **Cargo-like printing**
 //!
 //! Easily print beautiful formatted messages like Cargo does.
 //!
@@ -6,13 +6,24 @@
 //! ```rust
 //! use cprint::{cprint, Color};
 //!
-//! cprint!("Using", "cprint crate!" => Green); // "=> Green" is optional, it's the default color
 //! cprint!("Using cprint crate!");
 //! ```
+//!
+//! ## Coloration
+//! For all signatures, you can specify the color of the title with a predefined color from the [`colored::Color`] enum or with RGB values `(r, g, b)`.
+//! To specify the color use `=>` at the end of the strings.
+//!
+//! ## Macros
+//! - [`cprint!`] and [`cprintln!`] for printing to stdout.
+//! - [`ceprint!`] and [`ceprintln!`] for printing to stderr.
+//! - [`cformat!`] for formatting a string.
+
 
 pub use coloration::{Color, Coloration};
 
+#[doc(hidden)]
 pub mod coloration;
+
 #[cfg(feature = "cprint")]
 mod cprint;
 
